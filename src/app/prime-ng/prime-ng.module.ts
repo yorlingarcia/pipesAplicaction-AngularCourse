61549fb9506f97cabe81f42da9047de5c6e19bbc
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-23 09:10:18 -0500

    se creo las carpetas de los componentes y modulos

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//PrimeNg
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { MenubarModule } from 'primeng/menubar';
+
+@NgModule({
+  //declarations: [],
+  //imports: [CommonModule, ButtonModule, CardModule],
+  exports: [ButtonModule, CardModule, MenubarModule],
+})
+export class PrimeNgModule {}
