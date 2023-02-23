a2527f8d935201e76fffdd542986ce785ead6b21
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-23 14:03:00 -0500

    implementacion del pipe i18nSelected, se necsita tener un objeto de elementos a validar

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -3,9 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-no-comunes',
   templateUrl: './no-comunes.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NoComunesComponent {
+  nombre: string = 'Nancy';
+  genero: string = 'femenino';
 
+  invitacionMapa = {
+    masculino: 'invitarlo',
+    femenino: 'invitarla',
+  };
 }
