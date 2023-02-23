2d369025a4e392f185e8b9c40160f81680ec391e
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-22 21:56:12 -0500

    importacion del modulo de primeNg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+//PrimeNg
+import { ButtonModule } from 'primeng/button';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
