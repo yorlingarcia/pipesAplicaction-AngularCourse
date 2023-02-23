437ec554c9d230a7e03751b19ff5ee1af5f4cba8
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-23 10:15:28 -0500

    se integro el p-card a basicos, se tuvo que importar el modulo en ventas y el modulo de ventas en el app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
+
+import { VentasModule } from './ventas/ventas.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRouterModule, SharedModule],
+  imports: [BrowserModule, AppRouterModule, SharedModule, VentasModule],
   providers: [],
   bootstrap: [AppComponent],
 })
