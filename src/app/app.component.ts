e4591eb604310b6b0fd4c41f6ec27836af12a16a
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-22 21:55:32 -0500

    first commit: se crearon las dependencias y se instalo el PrimeNG con npm, fue necesario cerrar el servidor local

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipesApp';
+  nombre: string = 'Yorlin garcia';
+
+  mostrarNombre() {
+    console.log(this.nombre);
+  }
 }
