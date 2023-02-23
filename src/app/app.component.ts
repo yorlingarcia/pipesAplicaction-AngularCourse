6416d654e26dc1a35befcc3b5b112dd8704e5785
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-23 13:54:26 -0500

    integracion del ripple, efecto de burbuja/animacion)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,11 @@ export class AppComponent {
 
   mostrarNombre() {
     console.log(this.nombre);
+  }
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
   }
 }
