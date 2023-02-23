6d5a6be29c5e6c1bd15a3263f174e2d8b7010bb1
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-23 11:46:51 -0500

    uso de pipi con preferecia de datos

--- src/app/ventas/pages/basicos/basicos.component.ts
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -3,9 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class BasicosComponent {
+  nombreLower: string = 'yorlin';
+  nombreUpper: string = 'YORLIN';
+  nombreCompleto: string = 'yOrlIn GaRCIa';
 
+  fecha: Date = new Date();
 }
