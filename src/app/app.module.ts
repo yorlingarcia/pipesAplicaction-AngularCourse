39a351aa6945e066145ef97a6c98cc10675d48d2
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-23 09:05:12 -0500

    se cambio los imports de PrimeNg a un modulo aparte, tambien se uso un barmenu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
-//PrimeNg
-import { ButtonModule } from 'primeng/button';
-import { CardModule } from 'primeng/card';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ButtonModule, CardModule],
+  imports: [BrowserModule, SharedModule, PrimeNgModule],
   providers: [],
   bootstrap: [AppComponent],
 })
