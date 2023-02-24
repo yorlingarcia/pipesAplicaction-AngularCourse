25954be2feb7f1e1af8b318cfada1a8e28f3a5fd
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-23 22:15:06 -0500

    se agrego el evento de click para interactuar con las tarjetas

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 //PrimeNg
 import { ButtonModule } from 'primeng/button';
--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -9,8 +9,30 @@ export class NoComunesComponent {
   nombre: string = 'Nancy';
   genero: string = 'femenino';
 
+  //i18nSelect
   invitacionMapa = {
     masculino: 'invitarlo',
     femenino: 'invitarla',
   };
+  //i18nPlural
+  clientes: string[] = ['Maria', 'pedro', 'Yorlin', 'Osney'];
+  clientesMapa = {
+    '=0': 'no tenemos ningun cliente esperando',
+    '=1': ' tenemos 1 cliente esperando',
+    other: 'tenemos # clientes esperando', //el numeral permita colocar el numero
+  };
+
+  cambiarCliente() {
+    if (this.nombre === 'Nancy') {
+      this.nombre = 'Yorlin';
+      this.genero = 'masculino';
+    } else {
+      this.nombre = 'Nancy';
+      this.genero = 'femenino';
+    }
+  }
+
+  borrarCliente() {
+    this.clientes.shift();
+  }
 }
