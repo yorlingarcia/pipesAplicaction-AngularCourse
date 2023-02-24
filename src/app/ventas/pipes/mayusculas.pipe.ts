abef8ea26bbad5900bb5aed3b0e6d71e4c1892aa
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-24 11:34:27 -0500

    cambio del if'

--- src/app/ventas/pipes/mayusculas.pipe.ts
+++ src/app/ventas/pipes/mayusculas.pipe.ts
@@ -6,10 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class MayusculasPipe implements PipeTransform {
   transform(valor: string, enMayusculas: boolean = false): string {
     console.log(enMayusculas);
-    if (enMayusculas === true) {
-      return valor.toUpperCase();
-    } else {
-      return valor.toLowerCase();
-    }
+    return enMayusculas ? valor.toUpperCase() : valor.toLowerCase();
+    // if (enMayusculas === true) {
+    //   return valor.toUpperCase();
+    // } else {
+    //   return valor.toLowerCase();
+    // }
   }
 }
