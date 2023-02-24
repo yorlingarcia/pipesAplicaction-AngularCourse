9b6bedf8d1a1208667b99c33a61d2c9cb9228dd5
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-24 11:00:45 -0500

    creacion del archivo de pipes personalizados y su uso

---
+++ src/app/ventas/pipes/mayusculas.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'mayusculas',
+})
+export class MayusculasPipe implements PipeTransform {
+  transform(): string {
+    return 'HOLA MUNDO';
+  }
+}
